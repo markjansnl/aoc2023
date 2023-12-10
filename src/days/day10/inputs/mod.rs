cb1891dcf72aa2e689c219ed1a81d55e650fe7a9
Mark Jans <mark@markjans.nl>
2023-12-10 20:58:32 +0100

    Day 10

--- src/days/day10/inputs/mod.rs
+++ src/days/day10/inputs/mod.rs
@@ -1,8 +1,12 @@
 // GENERATED! DO NOT CHANGE!
 
-pub type Inputs = [&'static str; 3];
+pub type Inputs = [&'static str; 7];
 pub const INPUTS: Inputs = [
     include_str!("input.txt"),
     include_str!("example1.txt"),
     include_str!("example2.txt"),
+    include_str!("example3.txt"),
+    include_str!("example4.txt"),
+    include_str!("example5.txt"),
+    include_str!("example6.txt"),
 ];
--- src/days/day10/src/mod.rs
+++ src/days/day10/src/mod.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::{self, Debug},
+    iter::repeat,
+};
+
 use super::inputs::{Inputs, INPUTS};
 use crate::prelude::*;
 
@@ -31,38 +36,37 @@ impl Day for Day10 {
                     .collect()
             })
             .collect();
-        Ok(Map { map, start })
+
+        Ok(Map {
+            map,
+            start,
+            double: false,
+        })
     }
 
     fn part1(parsed: &Self::Parsed) -> Result<Self::Output> {
         let mut map = parsed.clone();
-        Ok(Day10::bfs_loop(&mut map))
+        let (depth, _) = Day10::bfs_loop(&mut map);
+        Ok(depth)
     }
 
     fn part2(parsed: &Self::Parsed) -> Result<Self::Output> {
-        let mut map = parsed.clone();
-        Day10::bfs_loop(&mut map);
-        for x in 0..map.map[0].len() as i32 {
-            map.flood(0, x);
-            map.flood(map.map.len() as i32 - 1, x);
-        }
-        for y in 1..map.map.len() as i32 - 1 {
-            map.flood(y, 0);
-            map.flood(y, map.map[0].len() as i32 - 1);
-        }
-
-        // 622 is too high
-
-        Ok(map.len())
+        let mut map = parsed.double();
+        let (_, last) = Day10::bfs_loop(&mut map);
+        map.close_loop(last);
+        map.flood_borders();
+        Ok(map.undouble().len())
     }
 }
 
 impl Day10 {
-    fn bfs_loop(map: &mut Map) -> i32 {
+    fn bfs_loop(map: &mut Map) -> (i32, ((i32, i32), char)) {
         let mut pipes = vec![(map.start, 'S')];
         let mut depth = -1;
-        while pipes.len() > 0 {
+        let mut last = Vec::new();
+        while !pipes.is_empty() {
             depth += 1;
+            last = pipes.clone();
             pipes = pipes
                 .into_iter()
                 .fold(Vec::new(), |mut pipes, ((y, x), c)| {
@@ -105,39 +109,126 @@ impl Day10 {
                     pipes
                 });
         }
-        depth
+        (depth, last[0])
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Map {
     map: Vec<Vec<Option<char>>>,
     start: (i32, i32),
+    double: bool,
 }
 
 impl Map {
     pub fn move_left(&mut self, y: i32, x: i32, pipes: &mut Vec<((i32, i32), char)>) {
-        self.move_checked(y, x - 1, pipes, "-LF")
+        if self.move_checked(y, x - self.one(), pipes, "-LF") && self.double {
+            self.take(y, x - 1);
+        }
     }
 
     pub fn move_right(&mut self, y: i32, x: i32, pipes: &mut Vec<((i32, i32), char)>) {
-        self.move_checked(y, x + 1, pipes, "-J7")
+        if self.move_checked(y, x + self.one(), pipes, "-J7") && self.double {
+            self.take(y, x + 1);
+        }
     }
 
     pub fn move_up(&mut self, y: i32, x: i32, pipes: &mut Vec<((i32, i32), char)>) {
-        self.move_checked(y - 1, x, pipes, "|7F")
+        if self.move_checked(y - self.one(), x, pipes, "|7F") && self.double {
+            self.take(y - 1, x);
+        }
     }
 
     pub fn move_down(&mut self, y: i32, x: i32, pipes: &mut Vec<((i32, i32), char)>) {
-        self.move_checked(y + 1, x, pipes, "|LJ")
+        if self.move_checked(y + self.one(), x, pipes, "|LJ") && self.double {
+            self.take(y + 1, x);
+        }
     }
 
-    pub fn flood(&mut self, y: i32, x: i32) {
-        if self.take(y, x).is_some() {
-            for (y, x) in [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)] {
-                self.flood(y, x)
+    pub fn double(&self) -> Map {
+        let mut new_map = Map {
+            map: Vec::with_capacity(self.map.len() * 2),
+            start: (self.start.0 * 2, self.start.1 * 2),
+            double: true,
+        };
+
+        for line in &self.map {
+            let line_len = line.len() * 2;
+            let mut new_line = Vec::with_capacity(line_len);
+            for option_c in line {
+                new_line.push(*option_c);
+                new_line.push(Some('.'));
+            }
+            new_map.map.push(new_line);
+            new_map
+                .map
+                .push(Vec::from_iter(repeat(Some('.')).take(line_len)));
+        }
+
+        new_map
+    }
+
+    pub fn close_loop(&mut self, last: ((i32, i32), char)) {
+        let ((y, x), c) = last;
+        match c {
+            '|' => {
+                self.take(y - 1, x);
+                self.take(y + 1, x);
+            }
+            '-' => {
+                self.take(y, x - 1);
+                self.take(y, x + 1);
+            }
+            'L' => {
+                self.take(y - 1, x);
+                self.take(y, x + 1);
+            }
+            'J' => {
+                self.take(y - 1, x);
+                self.take(y, x - 1);
+            }
+            '7' => {
+                self.take(y + 1, x);
+                self.take(y, x - 1);
+            }
+            'F' => {
+                self.take(y + 1, x);
+                self.take(y, x + 1);
+            }
+            _ => {
+                panic!("Unknown last character {c}");
             }
         }
+    }
+
+    pub fn flood_borders(&mut self) {
+        for x in 0..self.map[0].len() as i32 {
+            self.flood(0, x);
+            self.flood(self.map.len() as i32 - 1, x);
+        }
+        for y in 1..self.map.len() as i32 - 1 {
+            self.flood(y, 0);
+            self.flood(y, self.map[0].len() as i32 - 1);
+        }
+    }
+
+    pub fn undouble(self) -> Map {
+        let mut new_map = Map {
+            map: Vec::with_capacity(self.map.len() / 2),
+            start: (self.start.0 / 2, self.start.1 / 2),
+            double: false,
+        };
+
+        for (_, line) in self.map.iter().enumerate().filter(|(y, _)| y % 2 == 0) {
+            let line_len = line.len() / 2;
+            let mut new_line = Vec::with_capacity(line_len);
+            for (_, option_c) in line.iter().enumerate().filter(|(x, _)| x % 2 == 0) {
+                new_line.push(*option_c);
+            }
+            new_map.map.push(new_line);
+        }
+
+        new_map
     }
 
     pub fn len(&self) -> i32 {
@@ -147,19 +238,43 @@ impl Map {
             .count() as i32
     }
 
-    fn move_checked(&mut self, y: i32, x: i32, pipes: &mut Vec<((i32, i32), char)>, valid: &str) {
+    fn one(&self) -> i32 {
+        if self.double {
+            2
+        } else {
+            1
+        }
+    }
+
+    fn flood(&mut self, y: i32, x: i32) {
+        if self.take(y, x).is_some() {
+            for (y, x) in [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)] {
+                self.flood(y, x)
+            }
+        }
+    }
+
+    fn move_checked(
+        &mut self,
+        y: i32,
+        x: i32,
+        pipes: &mut Vec<((i32, i32), char)>,
+        valid: &str,
+    ) -> bool {
         if let Some(c) = self.get(y, x) {
             if valid.contains(c) {
                 pipes.push(((y, x), self.take(y, x).unwrap()));
+                return true;
             }
         }
+        false
     }
 
     fn get(&mut self, y: i32, x: i32) -> Option<char> {
         if x < 0 || y < 0 {
             None
         } else {
-            self.map.get(y as usize)?.get(x as usize)?.clone()
+            *self.map.get(y as usize)?.get(x as usize)?
         }
     }
 
@@ -169,5 +284,21 @@ impl Map {
         } else {
             self.map.get_mut(y as usize)?.get_mut(x as usize)?.take()
         }
+    }
+}
+
+impl Debug for Map {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for line in &self.map {
+            for option_c in line {
+                if let Some(c) = option_c {
+                    write!(f, "{c}")?;
+                } else {
+                    write!(f, " ")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
