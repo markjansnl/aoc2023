42c2760512b6df380d4559a688f07df659f945e7
Mark Jans <mark@markjans.nl>
2023-12-14 22:26:05 +0100

    Variable renamed

--- src/days/day14/src/mod.rs
+++ src/days/day14/src/mod.rs
@@ -68,12 +68,12 @@ pub struct Platform {
 }
 
 impl From<&<Day14 as Day>::Parsed> for Platform {
-    fn from(input: &<Day14 as Day>::Parsed) -> Self {
-        let width = input[0].len() as u8;
-        let height = input[1].len() as u8;
+    fn from(parsed: &<Day14 as Day>::Parsed) -> Self {
+        let width = parsed[0].len() as u8;
+        let height = parsed[1].len() as u8;
         let mut rocks = Vec::new();
 
-        for (y, line) in input.iter().enumerate() {
+        for (y, line) in parsed.iter().enumerate() {
             for (x, c) in line.iter().enumerate() {
                 match c {
                     '#' => rocks.push(Rock {
